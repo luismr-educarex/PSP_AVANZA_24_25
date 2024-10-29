f355e4157fa4e4e5cb213ea3d350d832f6f83cc7
Luis Martinez <lmartinezr10@educarex.es>
2024-10-29 13:26:09 +0100

    Ejemplo compartir hilos problema de los jardines

---
+++ src/unidades/unidad2/ejemplos/problemajardines/Entra_Jardin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidades.unidad2.ejemplos.problemajardines;
+
+
+public class Entra_Jardin extends Thread {
+//clase derivada de Thread que define un hilo
+    private RecursoJardin jardin;
+
+    public Entra_Jardin(String nombre, RecursoJardin j) {
+        this.setName(nombre);
+        this.jardin = j;
+    }
+
+    @Override
+    public void run() {
+        jardin.incrementaCuenta();
+        //invoca al método que incrementa la cuenta de accesos al jardín
+    }
+}
