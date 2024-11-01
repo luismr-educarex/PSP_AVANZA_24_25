e88c93386545042c965d19f602a9d9976407ce02
luismr-educarex <luismr@educarex.es>
2024-11-01 10:04:47 +0100

    Ejemplo interbloqueo

---
+++ src/unidades/unidad2/ejemplos/interbloqueo/Hilo1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidades.unidad2.ejemplos.interbloqueo;
+
+
+public class Hilo1 extends Thread {
+ //Declara dos objetos del tipos de cada fichero
+  FicheroA a;
+  FicheroB b;
+
+  /*****************************************************************************
+  constructor
+   *
+   * @param a: ficheroA
+   * @param b: ficheroB
+   */
+  Hilo1(FicheroA a, FicheroB b) {
+    this.a = a;
+    this.b = b;
+  }
+/*******************************************************************************
+ * Tarea del hilo: acceder al ficheroA
+ */
+  @Override
+  public void run() {
+    a.metodoA(b);
+    //el hilo accede al ficheroA
+  }
+}
